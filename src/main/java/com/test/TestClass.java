740e9aa00bee65681e4c646d1c6e7375dae7e415
Rajul Kukreja <rkukrej@amazon.com>
2021-12-29 04:12:41 +0530

    1st CLI commmit

--- src/main/java/com/test/TestClass.java
+++ src/main/java/com/test/TestClass.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Test Class");
+        System.out.println("Test Main Class");
     }
 }
